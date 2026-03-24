3dd74074d4a6b18fd8332f69a3a1879ce0109606
Harshitha Sathi <harshithasathi54@gmail.com>
2026-03-24 09:57:48 +0530

    Add files via upload

---
+++ StringOperations.java
@@ -0,0 +1,10 @@
+class Stringfunctions {
+    public static void main(String args[]) {
+        String name = "Mangam Tanmayi";
+
+        System.out.println("Substring is: " + name.substring(3, 8));
+        System.out.println("Lowercase is: " + name.toLowerCase());
+        System.out.println("Uppercase is: " + name.toUpperCase());
+        System.out.println("Concatenation is: " + name.concat(" Student"));
+    }
+}
